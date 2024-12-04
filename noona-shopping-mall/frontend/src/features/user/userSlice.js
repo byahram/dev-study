96caefbfe09fe4d404cdb8b630beaded50487965
Ahram Kim <45866137+byahram@users.noreply.github.com>
2024-12-04 23:02:55 +0900

    #9. 쇼핑몰 만들기 : 회원가입 프론트엔드

--- noona-shopping-mall/frontend/src/features/user/userSlice.js
+++ noona-shopping-mall/frontend/src/features/user/userSlice.js
@@ -20,7 +20,34 @@ export const registerUser = createAsyncThunk(
   async (
     { email, name, password, navigate },
     { dispatch, rejectWithValue }
-  ) => {}
+  ) => {
+    try {
+      const response = await api.post("/user", { email, name, password });
+
+      // 1. 성공 토스트 메시지 보여주기
+      dispatch(
+        showToastMessage({
+          message: "회원가입을 성공했습니다.",
+          status: "success",
+        })
+      );
+      // 2. 로그인 페이지로 리다이렉트
+      navigate("/login");
+
+      return response.data.data;
+    } catch (error) {
+      // 1. 실패 토스트 메시지 보여주기
+
+      dispatch(
+        showToastMessage({
+          message: "회원가입에 실패했습니다.",
+          status: "error",
+        })
+      );
+      // 2. 에러값을 저장한다
+      return rejectWithValue(error.message);
+    }
+  }
 );
 
 export const loginWithToken = createAsyncThunk(
@@ -37,13 +64,28 @@ const userSlice = createSlice({
     registrationError: null,
     success: false,
   },
+  // 직접적으로 아이템 호출될 떄
   reducers: {
     clearErrors: (state) => {
       state.loginError = null;
       state.registrationError = null;
     },
   },
-  extraReducers: (builder) => {},
+  // async처럼 외부의 함수를 통해 호출될 떄
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(registerUser.fulfilled, (state) => {
+        state.loading = false;
+        state.registrationError = null;
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.loading = false;
+        state.registrationError = action.payload;
+      });
+  },
 });
 export const { clearErrors } = userSlice.actions;
 export default userSlice.reducer;
