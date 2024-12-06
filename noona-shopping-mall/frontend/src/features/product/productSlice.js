65ebe8e5364a04e8a2a2ed2df700010a44edf40e
Ahram Kim <45866137+byahram@users.noreply.github.com>
2024-12-07 00:37:49 +0900

    #9. 쇼핑몰 만들기 : 상품생성 프론트엔드 완료

--- noona-shopping-mall/frontend/src/features/product/productSlice.js
+++ noona-shopping-mall/frontend/src/features/product/productSlice.js
@@ -15,7 +15,18 @@ export const getProductDetail = createAsyncThunk(
 
 export const createProduct = createAsyncThunk(
   "products/createProduct",
-  async (formData, { dispatch, rejectWithValue }) => {}
+  async (formData, { dispatch, rejectWithValue }) => {
+    try {
+      const response = await api.post("/product", formData);
+      if (response.status !== 200) throw new Error(response.error);
+      dispatch(
+        showToastMessage({ message: "상품 생성 완료", status: "success" })
+      );
+      return response.data.data;
+    } catch (err) {
+      return rejectWithValue(err.message);
+    }
+  }
 );
 
 export const deleteProduct = createAsyncThunk(
@@ -51,7 +62,23 @@ const productSlice = createSlice({
       state.success = false;
     },
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      // createProduct
+      .addCase(createProduct.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(createProduct.fulfilled, (state) => {
+        state.loading = false;
+        state.error = "";
+        state.success = true; // 상품 생성 (성공) 다이얼로그 닫고, (실패) 실패메시지 다이얼로그에 보여주고 닫지 않는다
+      })
+      .addCase(createProduct.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.success = false;
+      });
+  },
 });
 
 export const { setSelectedProduct, setFilteredList, clearError } =
