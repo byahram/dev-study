be17586607618f79aafd7c6e5d480ece5c4c8297
Ahram Kim <45866137+byahram@users.noreply.github.com>
2024-12-13 23:58:01 +0900

    #9. 쇼핑몰 만들기 : 오더 생성하기 프론트엔드

--- noona-shopping-mall/frontend/src/features/order/orderSlice.js
+++ noona-shopping-mall/frontend/src/features/order/orderSlice.js
@@ -16,7 +16,24 @@ const initialState = {
 // Async thunks
 export const createOrder = createAsyncThunk(
   "order/createOrder",
-  async (payload, { dispatch, rejectWithValue }) => {}
+  async (payload, { dispatch, rejectWithValue }) => {
+    try {
+      const response = await api.post("/order", payload);
+      if (response.status !== 200) throw new Error(response.error);
+
+      dispatch(getCartQty());
+      return response.data.orderNum;
+    } catch (err) {
+      dispatch(
+        showToastMessage({
+          message: err.message,
+          status: "error",
+        })
+      );
+
+      return rejectWithValue(err.message);
+    }
+  }
 );
 
 export const getOrder = createAsyncThunk(
@@ -43,7 +60,21 @@ const orderSlice = createSlice({
       state.selectedOrder = action.payload;
     },
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createOrder.pending, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(createOrder.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = "";
+        state.orderNum = action.payload;
+      })
+      .addCase(createOrder.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
 });
 
 export const { setSelectedOrder } = orderSlice.actions;
