113c2ac1ab469eea40622305c3e0e983e7b7639e
Ahram Kim <45866137+byahram@users.noreply.github.com>
2024-12-14 23:25:33 +0900

    #9. 쇼핑몰 만들기 : 오더 생성하기 백엔드

--- noona-shopping-mall/backend/routes/index.js
+++ noona-shopping-mall/backend/routes/index.js
@@ -4,10 +4,12 @@ const userRouter = require("./userRouter");
 const authRouter = require("./authRouter");
 const productRouter = require("./productRouter");
 const cartRouter = require("./cartRouter");
+const orderRouter = require("./orderRouter");
 
 router.use("/user", userRouter);
 router.use("/auth", authRouter);
 router.use("/product", productRouter);
 router.use("/cart", cartRouter);
+router.use("/order", orderRouter);
 
 module.exports = router;
