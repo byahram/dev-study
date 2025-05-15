bcd003c9b007a2bf04525002383ef4876335e079
Ahram Kim <45866137+byahram@users.noreply.github.com>
2025-05-15 14:01:44 +0900

    2일차 - 초급 - 데이터 형식 이해 및 문자열 조작 방법

---
+++ python/100 Days of Code - The Complete Python Pro Bootcamp/Day 2/Mathematical Operations/exercise.py
@@ -0,0 +1,9 @@
+height = 1.65 
+weight = 84
+
+# Write your code here.
+# Calculate the bmi using weight and height.
+# BMI는 해당 사람의 체중을 해당 사람의 키의 제곱으로 나눈 값
+bmi = weight / (height ** 2)
+
+print(bmi)
