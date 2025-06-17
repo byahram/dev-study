c5d7028b89856e001fe57a0aa569f04666706f44
Ahram Kim <45866137+byahram@users.noreply.github.com>
2025-06-18 00:15:18 +0900

    9일차 - 초급 - 딕셔너리, 네스팅 및 시크릿 옥션

---
+++ python/100 Days of Code - The Complete Python Pro Bootcamp/Day 9/Dictionaries/exercise.py
@@ -0,0 +1,23 @@
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+student_grades = {}
+
+for student, score in student_scores.items():
+    if score > 90:
+        grade = "Outstanding"
+    elif score > 80:
+        grade = "Exceeds Expectations"
+    elif score > 70:
+        grade = "Acceptable"
+    else:
+        grade = "Fail"
+
+    student_grades[student] = grade
+
+print(student_grades)
