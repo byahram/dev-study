8e49a8715621bbd7440fa45c098e0bd1648870ef
Ahram Kim <45866137+byahram@users.noreply.github.com>
2025-06-18 14:21:52 +0900

    10일차 - 초급 - 출력과 함수

--- python/100 Days of Code - The Complete Python Pro Bootcamp/Day 10/Calculator Project/main.py
+++ python/100 Days of Code - The Complete Python Pro Bootcamp/Day 10/Calculator Project/main.py
@@ -1,4 +1,49 @@
+import art
+
+# TODO: Write out the other 3 functions - subtract, multiply and divide
 def add(n1, n2):
     return n1 + n2
 
+def subtract(n1, n2):
+    return n1 - n2
 
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+# TODO: Add these 4 functions into a dictionary as the values. Key = "+", "-", "*", "/"
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+# print(operations["*"](4, 8))
+
+def calculator():
+    print(art.logo)
+    should_accumulate = True
+    num1 = float(input("What is the first number?: "))
+
+    while should_accumulate:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What is the next number?: "))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+
+        if choice == "y":
+            num1 = answer
+        else:
+            should_accumulate = False
+            print("\n" * 20)
+            calculator()
+
+
+calculator()
